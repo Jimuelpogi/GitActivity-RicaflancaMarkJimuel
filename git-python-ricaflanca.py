e8f67b4a6333a2b9967ab9fa509f5db77fdf6095
RicaflancaMarkJimuel <markjimuelricaflanca@gmail.com>
2024-02-15 21:25:56 +0800

    My Educational Background added

--- git-python-ricaflanca.py
+++ git-python-ricaflanca.py
@@ -3,3 +3,8 @@ print("My Personal Information:")
 print("Mark Jimuel Ricaflanca")
 print("Caigangan, Buenavista, Marinduque")
 print("April 28, 2001")
+
+print("My Educational Background:")
+
+print("CICS")
+print("BSI/T-2C")
